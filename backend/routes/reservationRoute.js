86756faadd42bf7fac2514025e13de00c47e2768
Parv-Agrawal26 <agrawalparv262004@gmail.com>
2024-07-16 12:24:56 +0530

    backend content

---
+++ backend/routes/reservationRoute.js
@@ -0,0 +1,5 @@
+import express from "express";
+import { sendReservation } from "../controller/reservation.js";
+const router = express.Router();
+router.post("/send",sendReservation);
+export default router;
