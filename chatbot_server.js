c298673e040b9e6d228543adb5258569dcb5f563
Parv-Agrawal26 <agrawalparv262004@gmail.com>
2024-07-18 18:12:32 +0530

    changes added

--- chatbot_server.js
+++ chatbot_server.js
@@ -9,7 +9,7 @@ const port = 3001;
 app.use(bodyParser.json());
 app.use(cors());
 
-const genAI = new GoogleGenerativeAI("AIzaSyCvEjYhG3b_tAjwggZFy9CF-_qFGIIqZzA");
+const genAI = new GoogleGenerativeAI("Your_API_Key");
 
 app.post('/generate', async (req, res) => {
     const prompt = req.body.prompt;
